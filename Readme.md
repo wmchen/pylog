b7ff9415423c5b2195da1a62fcdf956525cf32f6
Weiming Chen <wm_chen@yeah.net>
2024-04-03 16:04:17 +0800

    0.2.0

--- Readme.md
+++ Readme.md
@@ -1,38 +1,101 @@
-You can use this function to generate a logger to help you manage the logging outputs of your python project.
+You can use this module to generate a logger to help you manage the logging outputs of your python project.
 
 
 
 ## Installation
 
-Before use this function, you need to install `loguru` first.
+Before use this module, you need to install requirements by running the following command.
 
 ```bash
-pip install loguru
+pip install -r requirements.txt
 ```
 
 
 
-## Simple usage
+## Usage
 
-We have defined a default output format for you. Anyway, you can modify the format (content, color) by yourself.
+### Simple usage
+
+Import the module before using it.
 
 ```python
-from pylog import init_logger
+from pylog import Logger
+```
 
+An example of a simple usage is as follows.
 
-log = init_logger('{LOG_FILE_PATH}')
-log.info(f'This is a info')
-log.debug(f'This is a debug')
-log.warning(f'This is a warning')
-log.error(f'This is a error')
-log.critical(f'This is a critical')
+```python
+logger = Logger()
+logger.init_logger()
+logger.info('This is a info')
+logger.debug('This is a debug')
+logger.warning('This is a warning')
+logger.error('This is a error')
+logger.critical('This is a critical')
 ```
 
 ![simple_usage](./images/simple_usage.jpg)
 
+### Save log file
+
+You can specify a path to save the log file when you init the logger. The suffix of log file should be `.log`.
+
+```python
+logger = Logger()
+logger.init_logger('root_dir/logfile.log')
+```
+
+### Customize logging format
+
+We have defined a default output format for you, i.e. `{now_time} | {elapsed} | {level} | {message}`.
+
+The default output format setting is as follows.
+
+```python
+_default_format = dict(log_info=[dict(info="{extra[now_time]}", color="green"),
+                                 dict(info="{extra[elapsed]}", color="yellow"),
+                                 dict(info="{level}", color="level"),
+                                 dict(info="{message}", color="level")],
+                       separator=dict(symbol=" | ", color="red"))
+```
+
+Anyway, you can modify the format (content, color) by yourself. Please refer to [Loguru documentation](https://loguru.readthedocs.io/en/stable/index.html) for more details.
+
+### Time format
+
+Note that we defined the `now_time` and `elapsed` by the `extra` args in `logger.configure()` method.
+
+The default format of `now_time` is `%Y-%m-%d %H:%M:%S`, i.e. `{year}-{month}-{day} {hour}:{minute}:{second}`.
+
+You can specify the time format of `now_time` when you init the logger.
+
+```python
+logger = Logger()
+logger.init_logger('root_dir/logfile.log', timeformat='%Y.%m.%d %H:%M:%S')
+```
+
+Besides, the default format of `elapsed` is `%d day(s) %H:%M:%S`, i.e. `{day} day(s) {hour}:{minute}:{second}`. Noted that the time scale of `elapsed` only support `%d`-day, `%H`-hour, `%M`-minute, `%S`-second.
+
+You can specify the time format of `elapsed` when you init the logger.
+
+```python
+logger = Logger()
+logger.init_logger('root_dir/logfile.log', elapsedformat='%H:%M:%S')
+```
+
+### Timezone
+
+The default timezone is `Asia/Shanghai`. You can specify a timezone of `UTC` when you init the logger.
+
+```python
+logger = Logger()
+logger.init_logger('root_dir/logfile.log', timezone='UTC')
+```
+
+There are over 500 timezones supported by `pytz`, all the name of timezones are listed in `tutorial.ipynb`, FYI.
 
 
 ## More usage & Reference
 
-[Loguru documentation](https://loguru.readthedocs.io/en/stable/index.html)
+For more advanced usage of `Logger`, please refer to the source code and the [Loguru documentation](https://loguru.readthedocs.io/en/stable/index.html).
 
--- pylog.py
+++ pylog.py
@@ -4,37 +4,229 @@ Programmer: Weiming Chen
 Copyright © 2023-present Weiming Chen. All rights reserved.
 """
 import sys
-from typing import Optional
+import pytz
+from datetime import datetime
 from pathlib import Path
-from loguru import logger
+from typing import Dict, TextIO, Optional, Union
 
-__all__ = ['init_logger']
-
-
-_default_format = ('<green>{time:YYYY-MM-DD HH:mm:ss}</green>'
-                   ' <red>|</red> <yellow>elapsed: {elapsed}</yellow>'
-                   ' <red>|</red> <level>{level: <8}</level> <red>|</red>'
-                   ' <cyan>{file}</cyan> <cyan>{module}</cyan><red>:</red><cyan>{function}</cyan>'
-                   ' <red>[</red><cyan>{line}</cyan><red>]</red> <red>-</red> <level>{message}</level>')
+from loguru._logger import Logger as _Logger
+from loguru._logger import Core as _Core
 
 
-def init_logger(log_path: str | Path = '',
-                save_log_file: bool = True,
-                form: Optional[str] = _default_format,
-                **kwargs):
-    """
-    Initialize the logger
-    Args
-        log_path (str | Path): the path of log file, only be used when save_log_file=True
-        save_log_file (bool): whether to save the log file, default is True
-        form (optional, str): the format of logging output, default format is "_default_format"
-    """
-    logger.remove(handler_id=None)  # clear default settings
-    logger.add(sys.stdout, format=form)  # set the logging output of console
-    if save_log_file:
-        assert log_path.endswith('.log') or log_path.suffix == '.log', 'The suffix of log file must be ".log"'
-        log_path = Path(log_path)
-        if not log_path.parent.exists():
-            log_path.parent.mkdir(parents=True)
-        logger.add(log_path, format=form, **kwargs)  # set the logging output of log file
-    return logger
+FormatType = Union[Dict, str]
+
+
+class Logger:
+    _default_format = dict(log_info=[dict(info="{extra[now_time]}", color="green"),
+                                     dict(info="{extra[elapsed]}", color="yellow"),
+                                     dict(info="{level}", color="level"),
+                                     dict(info="{message}", color="level")],
+                           separator=dict(symbol=" | ", color="red"))
+    
+    def __init__(self, 
+                 format: Optional[FormatType] = None,
+                 quiet: bool = False):
+        self._default_format = self.auto_format(format)
+        self._quiet = quiet
+        self.logger = _Logger(core=_Core(),
+                              exception=None,
+                              depth=0,
+                              record=False,
+                              lazy=False,
+                              colors=False,
+                              raw=False,
+                              capture=True,
+                              patchers=[],
+                              extra={})
+        self.handlers = {}
+        self.save_path = None
+        self.start_time = None
+        self.timezone = None
+        self.timeformat = None
+
+    def init_logger(self, 
+                    save_path: Optional[str] = None,
+                    remove_default_handlers: Optional[bool] = True,
+                    screen_out: TextIO = sys.stderr, 
+                    level: str = "DEBUG",
+                    timezone: str = "Asia/Shanghai",
+                    timeformat: str = "%Y-%m-%d %H:%M:%S",
+                    elapsedformat: str = "%d day(s) %H:%M:%S", 
+                    **kwargs):
+        """init logger
+
+        Args:
+            save_path (Optional[str]): the path of log file. Defaults to None.
+            remove_default_handlers (Optional[bool]): whether to remove the default handlers. 
+                                                      Defaults to True.
+            screen_out (optional): the level of terminal output. Defaults to sys.stderr.
+            level (str): the level of logger. Defaults to "DEBUG".
+            timezone (str): timezone. Defaults to "Asia/Shanghai".
+            timeformat (str): datetime format. Defaults to "%Y-%m-%d %H:%M:%S".
+            elapsedformat (str): elapsed format. Defaults to "%d day(s) %H:%M:%S".
+        """
+        assert screen_out in [sys.stderr, sys.stdout]
+        assert level in ['TRACE', 'DEBUG', 'INFO', 'SUCCESS', 'WARNING', 'ERROR', 'CRITICAL']
+        if remove_default_handlers:
+            self.logger.remove(handler_id=None)
+            self.handlers = {}
+        
+        if self.start_time is None:
+            self.start_time = datetime.now()
+        if self.timezone != timezone:
+            self.timezone = timezone
+        if self.timeformat != timeformat:
+            self.timeformat = timeformat
+        self.logger.configure(extra=dict(now_time=None, elapsed=None),
+                              patcher=lambda record: record["extra"].update(now_time=self.get_now_time(self.timezone, self.timeformat),
+                                                                            elapsed=self.get_elapsed(elapsedformat)))
+
+        screen_out_handler_id = self.logger.add(screen_out, format=self._default_format, level=level)
+        self.handlers['screen_out'] = self.logger._core.handlers[screen_out_handler_id]
+        if save_path is not None:
+            save_path = Path(save_path)
+            assert save_path.suffix == '.log', 'The suffix of log file must be ".log"'
+            if not save_path.parent.exists():
+                save_path.parent.mkdir(parents=True)
+            save_path_handler_id = self.logger.add(save_path, format=self._default_format, level=level, **kwargs)
+            self.handlers['save_log'] = self.logger._core.handlers[save_path_handler_id]
+            self.save_path = str(save_path.absolute())
+
+    def auto_format(self, format):
+        def add(info, color):
+            return f"<{color}>{info}</{color}>"
+        
+        if format is None:
+            format = self._default_format.copy()
+        
+        if isinstance(format, str):
+            return format
+        elif isinstance(format, dict):
+            f = []
+            for i in format['log_info']:
+                f.append(add(i['info'], i['color']))
+            separator = add(format['separator']['symbol'], format['separator']['color'])
+            return f"{separator}".join(f)
+        else:
+            raise TypeError("only support 'str' and 'dict' type of format setting.")
+        
+    @staticmethod
+    def get_now_time(timezone, timeformat):
+        return datetime.now(tz=pytz.timezone(timezone)).strftime(timeformat)
+
+    def get_elapsed(self, elapsedformat):
+        def format_time(e, format):
+            total_seconds = int((e.total_seconds() * 10**6 - e.microseconds) / 10**6)
+            smallest_scale = None
+
+            if "%d" in format:
+                days = (total_seconds - e.seconds) // 86400
+                smallest_scale = "%d"
+            else:
+                days = 0
+
+            if "%H" in format:
+                hours = (total_seconds-(days*86400)) // 3600
+                smallest_scale = "%H"
+            else:
+                hours = 0
+
+            if "%M" in format:
+                minutes = (total_seconds-(days*86400)-(hours*3600)) // 60
+                smallest_scale = "%M"
+            else:
+                minutes = 0
+                
+            if "%S" in format:
+                seconds = total_seconds - (days*86400) - (hours*3600) - (minutes*60)
+                smallest_scale = "%S"
+            else:
+                seconds = 0
+
+            if smallest_scale == "%d":
+                remain_second = total_seconds - (days*86400)
+                if remain_second > 0:
+                    days += remain_second / 86400
+            
+            if smallest_scale == "%H":
+                remain_second = total_seconds - (days*86400) - (hours*3600)
+                if remain_second > 0:
+                    fraction = str(remain_second / 3600)
+                    return str(days), f"{hours:02d}.{fraction[2:]}", f"{minutes:02d}", f"{seconds:02d}"
+            
+            if smallest_scale == "%M":
+                remain_second = total_seconds - (days*86400) - (hours*3600) - (minutes*60)
+                if remain_second > 0:
+                    fraction = str(remain_second / 60)
+                    return str(days), f"{hours:02d}", f"{minutes:02d}.{fraction[2:]}", f"{seconds:02d}"
+            
+            return str(days), f"{hours:02d}", f"{minutes:02d}", f"{seconds:02d}"
+
+        elapsed = datetime.now() - self.start_time
+        days, hours, minutes, seconds = format_time(elapsed, elapsedformat)
+        
+        elapsed_text = ""
+        format_flag = False
+        format_symbol = ""
+        for ch in elapsedformat:
+            if ch == "%":
+                format_flag = True
+                format_symbol += "%"
+                continue
+
+            if format_flag:
+                format_symbol += ch
+                if format_symbol == "%d":
+                    elapsed_text += days
+                elif format_symbol == "%H":
+                    elapsed_text += hours
+                elif format_symbol == "%M":
+                    elapsed_text += minutes
+                elif format_symbol == "%S":
+                    elapsed_text += seconds
+                else:
+                    elapsed_text += format_symbol
+                format_symbol = ""
+                format_flag = False
+            else:
+                elapsed_text += ch
+                
+        return elapsed_text
+
+    def info(self, msg: str):
+        if not self._quiet:
+            self.logger.info(msg)
+
+    def success(self, msg: str):
+        if not self._quiet:
+            self.logger.success(msg)
+
+    def warning(self, msg: str):
+        if not self._quiet:
+            self.logger.warning(msg)
+
+    def error(self, msg: str):
+        if not self._quiet:
+            self.logger.error(msg)
+
+    def critical(self, msg: str):
+        if not self._quiet:
+            self.logger.critical(msg)
+
+    def debug(self, msg: str):
+        if not self._quiet:
+            self.logger.debug(msg)
+
+    def exception(self, msg: str):
+        if not self._quiet:
+            self.logger.exception(msg)
+            
+    def trace(self, msg: str):
+        if not self._quiet:
+            self.logger.trace(msg)
+
+    def set_quiet(self):
+        self._quiet = True
+
+    def set_activate(self):
+        self._quiet = False
